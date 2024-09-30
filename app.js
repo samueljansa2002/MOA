e50cdbbf31950fde85f08183ade092c14672a54e
Janša Samuel <L20220066@SSPTAJI.CZ>
2024-09-30 10:59:16 +0200

    kalkulackanenisracka

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ if ('serviceWorker' in navigator) {
 function sayHello() {
     document.getElementById('hello').innerText = 'Hello world';
 }
+function calculate() {
+    const operand1 = parseFloat(document.getElementById('operand1').value);
+    const operand2 = parseFloat(document.getElementById('operand2').value);
+    const operation = document.getElementById('operation').value;
+    var result;
+
+    if (isNaN(operand1) || isNaN(operand2)) {
+        result = "Zadejte prosím platná čísla.";
+    } else if((operand2 == 0) && (operation == 'divide')) {
+        result = "Nelze dělit nulou";
+    } else {
+        switch (operation) {
+            case 'add':
+                result = operand1 + operand2;
+                break;
+            case 'subtract':
+                result = operand1 - operand2;
+                break;
+            case 'multiply':
+                result = operand1 * operand2;
+                break;
+            case 'divide':
+                result = operand1 / operand2;
+                break;
+            default:
+                result = "Neznámá operace";
+        }
+    }
+
+    document.getElementById('result').innerText = "Výsledek: " + result;
+}
